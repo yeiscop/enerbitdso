4f39ffab140715b5f825de8d0e6a44b75078c9d2
pablete <jtamayoh@gmail.com>
2024-02-27 18:13:27 -0500

    update deprecated pydantic methods

--- src/enerbitdso/formats.py
+++ src/enerbitdso/formats.py
@@ -6,7 +6,7 @@ import pydantic
 
 
 def as_json(records: list[pydantic.BaseModel]) -> io.StringIO:
-    content = orjson.dumps([r.dict() for r in records])
+    content = orjson.dumps([r.model_dump() for r in records])
     res = io.BytesIO(content)
     wrapper = io.TextIOWrapper(res, encoding="utf-8")
     return wrapper
@@ -14,8 +14,8 @@ def as_json(records: list[pydantic.BaseModel]) -> io.StringIO:
 
 def as_csv(records: list[pydantic.BaseModel], header: bool) -> io.StringIO:
     res = io.StringIO(newline="")
-    fields = records[0].__fields__.keys()
-    content_lines = [r.dict() for r in records]
+    fields = records[0].model_fields.keys()
+    content_lines = [r.model_dump() for r in records]
     writer = csv.DictWriter(res, fields, lineterminator="\n")
     if header:
         writer.writeheader()
@@ -25,10 +25,8 @@ def as_csv(records: list[pydantic.BaseModel], header: bool) -> io.StringIO:
 
 
 def as_jsonl(records: list[pydantic.BaseModel]) -> io.StringIO:
-    content_lines = [orjson.dumps(r.dict()) for r in records]
-    res = io.BytesIO()
-    for i in content_lines:
-        res.write(i)
-        res.write(b"\n")
-    wrapper = io.TextIOWrapper(res, encoding="utf-8")
-    return wrapper
+    res = io.StringIO()
+    for i in records:
+        res.write(i.model_dump_json())
+        res.write("\n")
+    return res
